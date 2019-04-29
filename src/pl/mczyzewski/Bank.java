db2d8790d2abe5501572a96bb3b881ecde58c928
Michal Czyzewski <czy.mich@gmail.com>
2019-04-29 22:15:02 +0200

    add getters and setters for created fields

--- src/pl/mczyzewski/Bank.java
+++ src/pl/mczyzewski/Bank.java
@@ -2,5 +2,60 @@ package pl.mczyzewski;
 
 public class Bank
 {
-    
+    private int accountNumber;
+    private double balance;
+    private String name;
+    private String email;
+    private int phoneNumber;
+
+    public void setAccountNumber(int accountNumber)
+    {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getAccountNumber()
+    {
+        return accountNumber;
+    }
+
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setPhoneNumber(int phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPhoneNumber()
+    {
+        return phoneNumber;
+
+    }
 }
