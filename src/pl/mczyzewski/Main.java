ccf0c67fa06c353270171d9801f9016978ec525d
Michal Czyzewski <czy.mich@gmail.com>
2019-04-29 22:40:51 +0200

    add main klient and scanner

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -1,8 +1,19 @@
 package pl.mczyzewski;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        Bank klient = new Bank();
+        System.out.println("Enter klient name :");
+        String nameOfKlient = scanner.nextLine();
+        klient.setName(nameOfKlient);
+        scanner.close();
+        System.out.println("You enter name : " + klient.getName());
+
+
     }
 }
