292f81c6b532ffb7fc9f1af6236b014bce932fb2
Michal Czyzewski <czy.mich@gmail.com>
2019-04-30 23:07:09 +0200

    add constructors and default values

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -55,31 +55,42 @@ public class Main
 //        System.out.println(klient.getAccountInfo());
 //
 //
+//
+//        Account Michal = new Account();
+//        Michal.withdrawal(1000);
+//        Michal.deposit(50);
+//        Michal.deposit(51);
+//        Michal.withdrawal(100);
 
-        Account Michal = new Account();
-        Michal.withdrawal(1000);
-        Michal.deposit(50);
-        Michal.deposit(51);
-        Michal.withdrawal(100);
+//        Account anna = new Account("1", 2000, "Anna", "poczta@wp.pl", "1234321");
+//        System.out.println(anna.getNumber());
+//        System.out.println(anna.getBalance());
+//        System.out.println(anna.getCustomerName());
+//        System.out.println(anna.getCustomerEmailAddress());
+//        System.out.println(anna.getCustomerPhoneNumber());
+//
+//        System.out.println("++++++++++");
+//        Account kamil = new Account();
+//        System.out.println(kamil.getNumber());
+//        System.out.println(kamil.getBalance());
+//        System.out.println(kamil.getCustomerName());
+//        System.out.println(kamil.getCustomerEmailAddress());
+//        System.out.println(kamil.getCustomerPhoneNumber());
+//
+//        Account Mich = new Account("Mich","poczta@wo.pl","12342");
+//        System.out.println( Mich.getNumber() + " of "+Mich.getCustomerName() + " same " + Mich.getCustomerEmailAddress());
 
-        Account anna = new Account("1", 2000, "Anna", "poczta@wp.pl", "1234321");
-        System.out.println(anna.getNumber());
-        System.out.println(anna.getBalance());
-        System.out.println(anna.getCustomerName());
-        System.out.println(anna.getCustomerEmailAddress());
-        System.out.println(anna.getCustomerPhoneNumber());
+        VipCustomer vip = new VipCustomer();
+        System.out.println(vip.getCreditLimit() + " " + vip.emailAddress + " "+ vip.getName());
 
-        System.out.println("++++++++++");
-        Account kamil = new Account();
-        System.out.println(kamil.getNumber());
-        System.out.println(kamil.getBalance());
-        System.out.println(kamil.getCustomerName());
-        System.out.println(kamil.getCustomerEmailAddress());
-        System.out.println(kamil.getCustomerPhoneNumber());
+        System.out.println("--=======================--");
 
-        Account Mich = new Account("Mich","poczta@wo.pl","12342");
-        System.out.println( Mich.getNumber() + " of "+Mich.getCustomerName() + " same " + Mich.getCustomerEmailAddress());
+        VipCustomer vip2 = new VipCustomer("Poka","poczra@ww.pl");
+        System.out.println(vip2.getName() + " " + vip2.getEmailAddress() + " " + vip2.getCreditLimit());
 
+        System.out.println("--=======================--");
 
+        VipCustomer vip3 = new VipCustomer("Michał",2000,"polaczek@wp.pl");
+        System.out.println(vip3.getName() + " " + vip3.getCreditLimit() + " " + vip3.getEmailAddress());
     }
 }
